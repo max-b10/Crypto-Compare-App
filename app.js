12bf2b3edbf1eef89983d9b54ffef4e43097fe46
max-b10 <bungaymax@gmail.com>
2021-05-11 11:21:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,27 +2,45 @@ const tableBody = document.querySelector(".table-body");
 const mainContainer = document.querySelector(".container");
 axios
   .get(
-    "https:api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d"
   )
   .then((res) => {
     let allCurrencies = res.data;
     // console.log(allCurrencies);
     let cryptoCoin = "";
-    // Loop over each coin object in the data array:
+
+    // Loop over each coin object in the data array and
     allCurrencies.forEach((coin) => {
       cryptoCoin += "<tr>";
+      cryptoCoin += `<td> ${coin.market_cap_rank} </td>`;
       cryptoCoin += `<td> <img src="${coin.image}"> </td>`;
-      // cryptoCoin += `<td> ${coin.symbol} </td>`;
-      cryptoCoin += `<td> ${coin.name} </td>`;
-      cryptoCoin += `<td> £${coin.current_price}</td>`;
-      cryptoCoin += `<td> ${coin.price_change_percentage_24h}%</td>`;
-      cryptoCoin += `<td> ${coin.market_cap}</td>`;
-
-      // const coinName = coin.name;
-      // const coinDiv = document.createElement("div");
-      // coinDiv.innerHTML = coinName;
-      // mainContainer.appendChild(coinDiv);
+      cryptoCoin += `<td class="text-start">${coin.symbol.toUpperCase()}</td>`;
+      cryptoCoin += `<td class="text-start"> £${coin.current_price
+        .toFixed(2)
+        .toLocaleString()}</td>`;
+      if (coin.price_change_percentage_24h < 0) {
+        cryptoCoin += `<td class="text-danger"> ${coin.price_change_percentage_24h.toFixed(
+          2
+        )}%</td>`;
+      } else {
+        cryptoCoin += `<td class="text-success"> ${coin.price_change_percentage_24h.toFixed(
+          2
+        )}%</td>`;
+      }
+      if (coin.price_change_percentage_7d_in_currency < 0) {
+        cryptoCoin += `<td class="text-danger"> ${coin.price_change_percentage_7d_in_currency.toFixed(
+          2
+        )}%</td>`;
+      } else {
+        cryptoCoin += `<td class="text-success"> ${coin.price_change_percentage_7d_in_currency.toFixed(
+          2
+        )}%</td>`;
+      }
+      cryptoCoin += `<td> £${coin.market_cap.toLocaleString()}</td>`;
     });
+    // if (coin.price_change_percentage_24h < 0) {
+    //   coin.price_change_percentage_24h.display.style = "red";
+    // }
     document.getElementById("data").innerHTML = cryptoCoin;
   })
   .catch((e) => {
